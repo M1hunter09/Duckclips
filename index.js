073c5689e3d7f72083755aa16506e49afc9e8da0
M1hunter09 <m1hunter1509@gmail.com>
2026-06-24 15:50:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+//mobile scripts
+const hamburger = document.getElementById('hamburger');
+const navLinks  = document.querySelector('.nav-links');
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  hamburger.classList.toggle('open');
+});
+
+//pc scripts
+
+
+
 
